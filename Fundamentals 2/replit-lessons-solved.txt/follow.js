a49e9f45d2df13f98a62420efd87476733b4dc81
geor90 <giorgosreal2@gmail.com>
2021-06-09 13:41:25 +0300

    Solution for exercise Fundamentals-2

---
+++ Fundamentals 2/replit-lessons-solved.txt/follow.js
@@ -0,0 +1,11 @@
+const birthYear = 1948;
+const thisYear = 1965;
+const firstName = "Carlos";
+const lastName = "Stevenson";
+
+let fullName= firstName + " " + lastName ;
+let age = thisYear - birthYear ;
+
+const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old.";
+
+console.log(greeting);
