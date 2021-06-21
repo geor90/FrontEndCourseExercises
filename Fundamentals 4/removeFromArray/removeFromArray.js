28f9ea397706767840ab4c4d751d890b8b67d771
geor90 <giorgosreal2@gmail.com>
2021-06-21 11:17:05 +0300

    solutions fundamentals 4

--- Fundamentals 4/removeFromArray/removeFromArray.js
+++ Fundamentals 4/removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,...toRemove) {
+    let modArray = [...array];
+    for (let i=0; i<array.length; i++){
+      if (modArray.includes(toRemove[i])) {
+        modArray.splice(modArray.indexOf(toRemove[i]),1)
+      }
+    }
+     return modArray;
 }
 
 module.exports = removeFromArray
--- Fundamentals 4/reverseString/reverseString.js
+++ Fundamentals 4/reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
 }
-
+reverseString('hello');
 module.exports = reverseString
