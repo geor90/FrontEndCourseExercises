fdcee5064d17ac41662e5cf4c35dfb2cfb5a7b62
geor90 <giorgosreal2@gmail.com>
2021-06-27 13:03:30 +0300

    solution Etch-a-Sketch

---
+++ Etch-a-Sketch/Grid.js
@@ -0,0 +1,20 @@
+
+let grid = document.querySelector(".container") ;
+console.log(grid);
+
+function makeGrid(){
+    for (let i = 0 ; i<256; i++ ){
+      let div = document.createElement("div");
+    }
+    div.classList.add("square");
+    grid.appendChild("div");
+
+}
+
+
+function createNewGrid(){
+    
+}
+
+
+
